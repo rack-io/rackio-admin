bb3181837919cf725e6ae3e986bb129fc86f1e70
Nelson Carrasquel <carrasquel@outlook.com>
2020-08-31 21:03:51 -0400

    setup file added

---
+++ setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RackioAdmin",
+    version="0.1",
+    author="Nelson Carrasquel",
+    author_email="rackio.framework@outlook.com",
+    description="A Rackio extension to add a Web Admin Interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/rack-io/rackio-admin",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
