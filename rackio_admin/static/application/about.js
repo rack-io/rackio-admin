cff181faf963297462ec207c8e1246d5d5f881cc
Nelson Carrasquel <carrasquel@outlook.com>
2020-09-01 14:58:06 -0400

    About module added

---
+++ rackio_admin/static/application/about.js
@@ -0,0 +1,28 @@
+// about.js
+
+
+var about_app = new Tulipan({
+    template: {
+        url: "/admin/templates/about",
+        async: false
+    },
+
+    route: "/about",
+
+    data: {
+        trend: {},
+        query: {}
+    },
+
+    methods: {
+
+        after: function(){
+            
+        },
+
+        retrieveTrend: function(){
+
+            
+        }
+    }
+})
